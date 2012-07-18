184b1e1c7baaed88f04a744967f4e3fbb3a2a405
Rishabh Rao <rishabhsrao@yahoo.com>
2012-07-18 12:46:51 +0530

    Completed v1

--- intermediate/js/scripts-concat.js
+++ intermediate/js/scripts-concat.js
@@ -1,1 +1,1 @@
-jQuery(document).ready(function(){jQuery("[title]").tooltip()});
+jQuery(document).ready(function(){jQuery("[rel='tooltip']").tooltip();jQuery("[rel='popover']").popover({html:!0,placement:"right"})});
